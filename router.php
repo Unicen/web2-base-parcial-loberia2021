79c845c63029b5f6750f1a59de090eedbffa8460
Lautaro <lautaroemanuelvalenzuela@gmail.com>
2021-06-10 16:47:03 -0300

    First commit

---
+++ router.php
@@ -0,0 +1,26 @@
+<?php
+    //require_once 'controllers/example.controller.php';
+
+    // base url
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+    // define una acción por defecto
+    if (empty($_GET['action'])) {
+        $_GET['action'] = 'home';
+    } 
+
+    // toma la acción que viene del usuario y parsea los parámetros
+    $accion = $_GET['action']; 
+    $parametros = explode('/', $accion);
+
+    // TABLA DE RUTEO
+    switch ($parametros[0]) {
+        case 'home': 
+            echo "home";
+        break;
+        
+        default:  
+            echo "404";
+        break;
+        
+    }
